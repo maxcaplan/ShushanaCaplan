ee659ee1b7bf9424e2abfa6f35df4ecaf3bcb415
maxacaplan <maxacaplan@gmail.com>
2021-07-20 15:14:36 -0300

    home page layout added

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,157 @@
 <template>
-  <div id="home"></div>
+  <div id="home">
+    <!-- Main Banner -->
+    <div class="w-full py-14 md:py-24 bg-gray-400">
+      <div class="container mx-auto px-3 sm:px-0">
+        <p class="text-5xl md:text-7xl font-bold text-white text-shadow-md">
+          Shushana <br />
+          Caplan
+        </p>
+      </div>
+    </div>
+
+    <div class="container mx-auto pt-6 pb-3 px-3 sm:px-0 space-y-12">
+      <div class="flex flex-col sm:flex-row space-y-4 sm:space-x-4">
+        <div class="flex-1 flex-col space-y-4">
+          <p class="inline text-3xl font-bold border-b-2 border-gray-500">
+            Contact
+          </p>
+
+          <p>
+            <b>Email:</b> <br />
+            shushanacaplan@hotmail.com
+          </p>
+
+          <p>
+            <b>Phone:</b> <br />
+            514-629-9761
+          </p>
+
+          <div class="flex flex-row space-x-4">
+            <!-- Facebook Button -->
+            <a
+              href="#"
+              class="rounded-md bg-gray-100 fill-current text-gray-700 p-2 hover:bg-blue-500 hover:text-gray-50"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                class="h-6 w-6"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
+                />
+              </svg>
+            </a>
+
+            <!-- Instagram Button -->
+            <a
+              href="#"
+              class="rounded-md bg-gray-100 fill-current text-gray-700 p-2 hover:bg-red-500 hover:text-gray-50"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                class="h-6 w-6"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
+                />
+              </svg>
+            </a>
+
+            <!-- Etsy Button -->
+            <a
+              href="#"
+              class="rounded-md bg-gray-100 fill-current text-gray-700 p-2 hover:bg-yellow-500 hover:text-gray-50"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                class="h-6 w-6"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="m 9.8240183,6.0129376 v 5.5407074 c 0,0 1.9523427,0 2.9972197,-0.07931 0.8217,-0.143867 0.972944,-0.223178 1.124188,-1.044877 l 0.302489,-1.2034993 h 0.893632 l -0.151244,2.6301753 0.07931,2.694731 h -0.90101 L 13.945426,13.49861 c -0.223177,-0.748843 -0.525666,-0.901009 -1.124188,-0.972943 -0.748844,-0.07931 -2.9972197,-0.07931 -2.9972197,-0.07931 v 4.647075 c 0,0.893632 0.4537337,1.275431 1.4986097,1.275431 h 3.148464 c 0.972944,0 1.944965,-0.07931 2.550865,-1.49861 l 0.814321,-1.801098 H 18.5925 c -0.07931,0.374421 -0.453733,3.674131 -0.525666,4.416518 0,0 -2.845976,-0.07193 -4.042097,-0.07193 H 8.6278971 l -3.2203973,0.07193 v -0.814322 l 1.0448769,-0.230555 c 0.7562216,-0.151244 0.9793992,-0.374422 0.9793992,-0.972944 0,0 0.079311,-2.024276 0.079311,-5.396839 0,-3.3642645 -0.079311,-5.3894628 -0.079311,-5.3894628 0,-0.670455 -0.2231776,-0.8216993 -0.9793992,-0.9729436 L 5.4074998,5.4872714 V 4.6655721 l 3.1484641,0.064555 h 5.9944391 c 1.196122,0 3.21302,-0.2157998 3.21302,-0.2157998 0,0 -0.07193,1.2680545 -0.151244,4.2652742 h -0.8217 L 16.487991,7.7273473 C 16.19288,6.3799818 15.745602,5.7095268 14.917448,5.7095268 h -4.71163 C 9.8240183,5.710449 9.8240183,5.7823823 9.8240183,6.0129376 Z"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        <!-- Contact Form -->
+        <div class="hidden sm:flex flex-1 flex-col lg:mw-half space-y-4">
+          <div class="flex flex-row space-x-3">
+            <input
+              type="text"
+              name="email"
+              class="block w-full px-3 py-2 rounded-md border border-solid border-gray-300 focus:ring-2"
+              placeholder="Email *"
+            />
+
+            <input
+              type="text"
+              name="subject"
+              class="block w-full px-3 py-2 rounded-md border border-solid border-gray-300 focus:ring-2"
+              placeholder="Subject *"
+            />
+          </div>
+
+          <textarea
+            name="body"
+            rows="3"
+            class="resize-none w-full px-3 py-2 rounded-md border border-solid border-gray-300 focus:ring-2"
+            placeholder="Message *"
+          ></textarea>
+
+          <button
+            class="rounded-md bg-gray-100 fill-current text-gray-700 p-2 hover:bg-gray-200"
+          >
+            Send
+          </button>
+        </div>
+      </div>
+
+      <div class="flex flex-col space-y-4">
+        <div>
+          <p class="inline text-3xl font-bold border-b-2 border-gray-500">
+            Recent Work
+          </p>
+        </div>
+
+        <!-- TODO: Add recent works -->
+
+        <div class="flex justify-center">
+          <a
+            href="#"
+            class="flex flex-row space-x-3 items-center rounded-md bg-white border border-gray-300 fill-current text-gray-700 py-4 px-6 hover:bg-gray-100"
+          >
+            <p class="font-bold">
+              See More
+            </p>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
