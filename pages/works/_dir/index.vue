dec2c3b71c1b1a39658b8b1a1fe586e97cfdbb83
maxacaplan <maxacaplan@gmail.com>
2021-09-04 11:29:55 -0300

    added back button to collection page

--- pages/works/_dir/index.vue
+++ pages/works/_dir/index.vue
@@ -1,9 +1,31 @@
 <template>
   <div id="work" class="container mx-auto p-3 sm:px-0 space-y-5">
-    <div>
+    <div class="flex flex-row space-x-3">
       <p class="inline text-3xl font-bold border-b-2 border-gray-300">
         Abstract Landscape
       </p>
+
+      <div class="flex flex-grow justify-end">
+        <button
+          @click="$router.go(-1)"
+          class="flex flex-row space-x-3 items-center py-3 px-5 rounded hover:bg-gray-100"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+
+          <p>Back</p>
+        </button>
+      </div>
     </div>
 
     <div class="grid grid-cols-3 gap-4">
